3294ddff3e570928d6cf61e978036beb86675930
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-01-18 13:19:46 +0545

    Motor class init method

--- MotorModule.py
+++ MotorModule.py
@@ -11,3 +11,13 @@ class Motor():
         self.EnaB = EnaB
         self.In1B = In1B
         self.In2B = In2B
+        GPIO.setup(self.EnaA, GPIO.OUT)
+        GPIO.setup(self.In1A, GPIO.OUT)
+        GPIO.setup(self.In2A, GPIO.OUT)
+        GPIO.setup(self.EnaB, GPIO.OUT)
+        GPIO.setup(self.In1B, GPIO.OUT)
+        GPIO.setup(self.In2B, GPIO.OUT)
+        self.pwmA = GPIO.PWM(self.EnaA, 100);
+        self.pwmA.start(0);
+        self.pwmB = GPIO.PWM(self.EnaB, 100);
+        self.pwmB.start(0);
