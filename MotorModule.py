bb7aa8b46e9c2e2214288c758643205ae91b9df4
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-01-18 08:28:35 +0545

    Motor class start

--- MotorModule.py
+++ MotorModule.py
@@ -2,3 +2,12 @@ import RPi.GPIO as GPIO
 from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+class Motor():
+    def __init__(self,EnaA,In1A,In2A,EnaB,In1B,In2B):
+        self.EnaA = EnaA
+        self.In1A = In1A
+        self.In2A = In2A
+        self.EnaB = EnaB
+        self.In1B = In1B
+        self.In2B = In2B
