8f91184e385f2548703a20539f75a8327b5f01d6
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-02 08:15:41 +0545

    key press module getKey

--- KeyPressModule.py
+++ KeyPressModule.py
@@ -4,3 +4,14 @@ def init():
     pygame.init()
     win = pygame.display.set_mode((100, 100))
 
+def getKey(keyName):
+    ans = False
+    for eve in pygame.event.get():pass
+    keyInput = pygame.key.get_pressed()
+    myKey = getattr(pygame, 'K_{}'.format(keyName))
+    if keyInput [myKey]:
+        ans = True
+        print('key a awas pressed')
+    pygame.display.update()
+    return ans
+
