0529042f99c224a24222e9843a370f3d9f09355c
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-05 07:14:26 +0545

    robot main function start

--- RobotMain.py
+++ RobotMain.py
@@ -2,16 +2,7 @@ from MotorModule import Motor
 
 motor = Motor(2, 3, 4, 17, 22, 27)
 
-
-def main():
-    motor.move(0.6, 0, 2)
-    motor.stop(2)
-    motor.move(-0.5, 0.2, 2)
-    motor.stop(2)
-
-
-
-
+import KeyPressModule as kp
 
 
 
