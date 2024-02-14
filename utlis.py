6ad983c46b25102ec6a8d8f98fc4b389fc1c0f8d
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-14 07:04:05 +0545

    utlis imports threshold function

--- utlis.py
+++ utlis.py
@@ -3,3 +3,4 @@ import numpy as np
 
 def thresholding(img):#thresholding function
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)  # converts BGR image to HSV
+    lowerWhite = np.array([85, 0, 0])  # lower bound of white color
