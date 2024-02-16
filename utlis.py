5ca8a4783570c4dea5660d7a7c5b74b95b41fa94
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-16 07:01:43 +0545

    utlis imports warpImg function

--- utlis.py
+++ utlis.py
@@ -13,3 +13,5 @@ def warpImg (img,points,w,h,inv=False):
     pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
     if inv:
         matrix = cv2.getPerspectiveTransform(pts2, pts1)
+    else:
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
