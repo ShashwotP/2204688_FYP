64b3227949cf97d54152be8e3fe0b64d97742d4b
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-16 07:01:24 +0545

    utlis imports warpImg function

--- utlis.py
+++ utlis.py
@@ -11,3 +11,5 @@ def thresholding(img):#thresholding function
 def warpImg (img,points,w,h,inv=False):
     pts1 = np.float32(points)
     pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
+    if inv:
+        matrix = cv2.getPerspectiveTransform(pts2, pts1)
