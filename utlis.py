f6d8dc986af1d86d197f46a254b6ea4a8ebcb47e
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-17 08:07:15 +0545

    utlis imports nothing function

--- utlis.py
+++ utlis.py
@@ -17,3 +17,6 @@ def warpImg (img,points,w,h,inv=False):
         matrix = cv2.getPerspectiveTransform(pts1, pts2)
     imgWarp = cv2.warpPerspective(img, matrix, (w, h))
     return imgWarp
+
+def nothing(a):
+    pass
