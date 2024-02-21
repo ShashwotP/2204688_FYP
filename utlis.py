fc10e9f2aca9ecfa7ce663d3ed53632a5784f715
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-21 07:13:58 +0545

    utlis imports getHistogram function

--- utlis.py
+++ utlis.py
@@ -43,3 +43,5 @@ def drawPoints(img,points):
     for x in range( 0,4):
         cv2.circle(img, (int(points[x][0]), int(points[x][1])), 15, (0, 0, 255), cv2.FILLED)
     return img
+
+def getHistogram(img, display=False, minPer=0.1, region=4):
