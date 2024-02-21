78d5ae697430dd21156fe1d01cd717b35065339c
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-21 07:15:35 +0545

    utlis imports getHistogram function

--- utlis.py
+++ utlis.py
@@ -49,4 +49,6 @@ def getHistogram(img, display=False, minPer=0.1, region=4):
         histValues = np.sum(img, axis=0)
     else:
         histValues = np.sum(img[img.shape[0] // region:, :], axis=0)
+        # histValues = np.sum(img, axis=0)
+        # print(histValues)
 
