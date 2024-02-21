96ad67faf26cfd139246e1511a13a19a3a7a084d
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-21 07:15:54 +0545

    utlis imports getHistogram function

--- utlis.py
+++ utlis.py
@@ -51,4 +51,5 @@ def getHistogram(img, display=False, minPer=0.1, region=4):
         histValues = np.sum(img[img.shape[0] // region:, :], axis=0)
         # histValues = np.sum(img, axis=0)
         # print(histValues)
+        maxValue = np.max(histValues)  # FIND THE MAX VALUE
 
