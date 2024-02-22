c9d14b5f0b270510d5f75ad3a56f721cdc380e22
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-22 07:16:18 +0545

    utlis imports getHistogram function

--- utlis.py
+++ utlis.py
@@ -69,4 +69,5 @@ def getHistogram(img, display=False, minPer=0.1, region=4):
                 ##intensity // 255
             print('Value', (type(x), type(int(img.shape[0] - (intensity // 255 // region)))))
             cv2.line(imgHist, (x, img.shape[0]), (x, img.shape[0] - (intensity // 255 // region)), color, 1)
+        cv2.circle(imgHist, (basePoint, img.shape[0]), 20, (0, 255, 255), cv2.FILLED)
 
