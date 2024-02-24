2662bc1a8e72a4d25824ac652e05398defea69cf
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-24 07:05:58 +0545

    utlis imports stackImages function

--- utlis.py
+++ utlis.py
@@ -81,4 +81,5 @@ def stackImages(scale,imgArray):
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-
+        for x in range(0, rows):
+            for y in range(0, cols):
