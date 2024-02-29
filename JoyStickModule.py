bca33b90e5379495939581ed24fd57833fe7e599
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-02-29 06:57:35 +0545

    JoyStickModule initialize

--- JoyStickModule.py
+++ JoyStickModule.py
@@ -7,3 +7,4 @@ controller.init()
 buttons = {'x':0,'o':0,'s':0,'t':0,
             'share':0,'R1':0,'options':0,'R2':0,
             'L2':0,'L1':0,
+           'axis1':0.,'axis2':0.,'axis3':0.,'axis4':0.}
