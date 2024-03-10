b4443be3ff3d7558189a52c33c87d7c766e13d92
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-03-10 09:20:04 +0545

    DataCollectionModule saveLog

--- DataCollectionModule.py
+++ DataCollectionModule.py
@@ -38,3 +38,4 @@ def saveLog():
                'Steering': steeringList}
     df = pd.DataFrame(rawData)
     df.to_csv(os.path.join(myDirectory, f'log_{str(countFolder)}.csv'), index=False, header=False)
+    print('Log Saved')
