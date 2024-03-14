15e594a67fc9ba2634b9c85fae2549aa00757d1f
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-03-14 07:00:33 +0545

    Training training data

--- Training.py
+++ Training.py
@@ -23,3 +23,4 @@ imagesPath, steerings = loadData(path,data)
 xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings,
                                               test_size=0.2,random_state=10)
 print('Total Training Images: ',len(xTrain))
+print('Total Validation Images: ',len(xVal))
