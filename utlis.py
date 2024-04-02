f3e27ec0374ceefbd08ea792e1a05361656d46e5
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-02 06:42:48 +0545

    utlis start augmentImage

--- utlis.py
+++ utlis.py
@@ -93,6 +93,7 @@ def augmentImage(imgPath,steering):
     img = mpimg.imread(imgPath)
     if np.random.rand() < 0.5:
         pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+        img = pan.augment_image(img)
 
 
 
