31d03b36d5053990324364356cdb7c3a2f78c63e
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-03 07:00:31 +0545

    utlis start augmentImage

--- utlis.py
+++ utlis.py
@@ -97,6 +97,7 @@ def augmentImage(imgPath,steering):
     if np.random.rand() < 0.5:
         zoom = iaa.Affine(scale=(1, 1.2))
         img = zoom.augment_image(img)
+    if np.random.rand() < 0.5:
 
 
 
