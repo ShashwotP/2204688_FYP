afdec749182da349325e215793b774f6a683a48c
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-14 05:51:31 +0545

    runmain preProcess

--- RunMain.py
+++ RunMain.py
@@ -14,3 +14,5 @@ motor = mM.Motor(2, 3, 4, 17, 22, 27) # Pin Numbers
 model = load_model('C:/Users/Dell/PycharmProjects/Trial/model.h5')
 #model1 = load_model()
 ######################################
+
+def preProcess(img):
