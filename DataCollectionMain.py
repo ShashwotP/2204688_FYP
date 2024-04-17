407b45506e5d32e23161ecbfabe899eaecf95de1
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-17 06:58:52 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -1,5 +1,6 @@
 import WebcamModule as wM #importing WebcamModule as wM
 import DataCollectionModule as dcM #importing DataCollectionModule as dcM
+import JoyStickModule as jsM #importing JoyStickModule as jsM
 
 
 
