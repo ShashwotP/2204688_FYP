973e59df71614a7da41b267d816e3b3292d52df9
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-18 07:07:48 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -2,7 +2,7 @@ import WebcamModule as wM #importing WebcamModule as wM
 import DataCollectionModule as dcM #importing DataCollectionModule as dcM
 import JoyStickModule as jsM #importing JoyStickModule as jsM
 import MotorModule as mM #importing MotorModule as mM
-import cv2 #importing OpenCV library
+
 
 
 
