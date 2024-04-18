53362f7eb3d4a8399c9339db83a0208c463579bd
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-18 07:08:14 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -3,6 +3,7 @@ import DataCollectionModule as dcM #importing DataCollectionModule as dcM
 import JoyStickModule as jsM #importing JoyStickModule as jsM
 import MotorModule as mM #importing MotorModule as mM
 import cv2 #importing OpenCV library
+from time import sleep #importing the sleep function from time module
 
 
 
