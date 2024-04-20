4455fa13bad1f84ee7e9ae6190cfe04632e9e8a3
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-20 07:37:44 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -15,6 +15,7 @@ while True: #start infinite loop
     # print(joyVal)
     steering = joyVal['axis1']  # get steering value from joystick
     throttle = joyVal['o'] * maxThrottle  # get throttle value from joystick and scale it
+    if joyVal['share'] == 1:  # if the 'share' button is pressed
 
 
 
