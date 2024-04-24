4ab2b6a87ae8e6dd3fdaa0d1610a0a2cbeaad1b9
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-04-24 06:06:16 +0545

    datacollectionmodule comments

--- DataCollectionModule.py
+++ DataCollectionModule.py
@@ -7,4 +7,5 @@ from datetime import datetime #importing datetime module
 global imgList, steeringList #declaring global variables
 countFolder = 0 #initializing folder count variable
 count = 0 #initializing count variable
+imgList = [] #initializing empty list to store images
 
