c7b0441b6abf1c934508df4658d7893f1abfece6
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-05-05 06:15:13 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -14,6 +14,7 @@ record = 0 #initialize record variable
 while True: #start infinite loop
     joyVal = jsM.getJS()  # get joystick values
     # print(joyVal)
+    steering = joyVal['axis1']  # get steering value from joystick
 
 
 
