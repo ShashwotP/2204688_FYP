e6da4e959212d3daf9ca76e3ad27d6304eb9e16a
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-05-09 06:01:30 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -39,4 +39,9 @@ while True: #start infinite loop
             # import cv2
             # from datetime import datetime
             #
+# global imgList, steeringList
+            # countFolder = 0
+            # count = 0
+            # imgList = []
+            # steeringList = []
 
