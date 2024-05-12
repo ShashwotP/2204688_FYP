60cbb8bb94ba21c67329113814e5091bb38033d9
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-05-12 05:47:09 +0545

    datacollectionmain comments

--- DataCollectionMain.py
+++ DataCollectionMain.py
@@ -75,4 +75,5 @@ while True: #start infinite loop
             #     df = pd.DataFrame(rawData)
  #     df.to_csv(os.path.join(myDirectory,f'log_{str(countFolder)}.csv'), index=False, header=False)
             #     print('Log Saved')
-
+ #     print('Total Images: ',len(imgList))
+            #
