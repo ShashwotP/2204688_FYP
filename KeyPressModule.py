24198bbe0ac4699d9df3aa09eed5c6c0e420d0ce
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-05-15 06:36:35 +0545

    Keypressmodule comments

--- KeyPressModule.py
+++ KeyPressModule.py
@@ -8,3 +8,5 @@ def init():# init method to initialize pygame and set up window
 def getKey(keyName):  # function getKey
     ans = False
     for eve in pygame.event.get(): pass
+    keyInput = pygame.key.get_pressed()  # retrieves current state of all keyboard keys
+    myKey = getattr(pygame, 'K_{}'.format(
