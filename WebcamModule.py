1f60b1ab5c0ab1c7af10987b9020207bf93fdb38
Shashwot Pradhan <np03cs4a210008@heraldcollege.edu.np>
2024-05-21 07:28:24 +0545

    Webcamodule comments

--- WebcamModule.py
+++ WebcamModule.py
@@ -1,5 +1,6 @@
 import cv2 #importing OpenCV library
 frameWidth = 640 #width of the frame
 frameHeight = 480 #height of the frame
+cap = cv2.VideoCapture(0) #initialize video capture from webcam
 
 
